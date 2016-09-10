e6c28ae205a771b52f560cec19ac78b063aa80fc
arkweid <a.a.abroskin@yandex.ru>
2016-09-10 14:37:50 +0300

    replace .switch_best test

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Answer, type: :model do
   it { should validate_presence_of :question_id }
   it { should validate_presence_of :content }
   it { should validate_length_of(:content).is_at_least(10).is_at_most(1000) }
+
+  describe 'Check answer model methods' do
+    let(:user) { create :user }
+    let(:question) { create :question, user: user }
+    let(:answer) { create :answer, question: question, user: user }
+
+    it '.switch_best toggle answer.best' do
+      expect(answer.best).to eq false
+      answer.switch_best
+      expect(answer.best).to eq true
+    end
+  end
 end
