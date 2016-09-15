cc7f532b53ee5bd4764d53eee1c8b99ef1eedc00
Arkweid <a.a.abroskin@yandex.ru>
2016-09-15 10:19:26 +0300

    acceptance test for several files to question

--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -21,4 +21,21 @@ feature 'Add files to question', '
 
     expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
   end
+
+  scenario 'User add several files when asks question', js: true do
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Content', with: 'text text text'
+    click_on 'add file'
+
+    within(all(:css, '.nested-fields').first) do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    end
+    within(all(:css, '.nested-fields').last) do
+      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+    end
+    click_on 'Create'
+
+    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+  end  
 end
