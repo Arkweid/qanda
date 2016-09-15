591217e5215b1e3d4d963909ae2f82191c237782
arkweid <a.a.abroskin@yandex.ru>
2016-09-15 21:35:45 +0300

    refactor

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -6,53 +6,66 @@ feature 'Add files to answer', '
   I`d like to be able to attach files
 ' do
 
-  given(:user) { create(:user) }
-  given(:question) { create(:question) }
+  given(:user) { create :user }
+  given(:question) { create :question }
 
-  background do
-    sign_in(user)
-    visit question_path(question)
-  end
-
-  scenario 'User adds file to answer', js: true do
-    fill_in 'Content', with: 'My answer with file'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Add answer'
-
-    within '.answers' do
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+  describe 'Owner user try' do
+    before do
+      sign_in(user)
+      visit question_path(question)
+      fill_in 'Content', with: 'My answer with file'
     end
-  end
 
-  scenario 'User adds file to answer', js: true do
-    fill_in 'Content', with: 'My answer with file'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Add answer'
-
-    click_on 'Delete file'
-
-    within '.answers' do
-      expect(page).to_not have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-    end
-  end
-
-  scenario 'non owner not see delete_link'
-
-  scenario 'User add several files when make asnwer', js: true do
-    fill_in 'Content', with: 'My answer with file'
-    click_on 'add file'
-
-    within(all(:css, '.nested-fields').first) do
+    scenario 'add file to answer', js: true do
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    end
-    within(all(:css, '.nested-fields').last) do
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
-    end
-    click_on 'Add answer'
+      click_on 'Add answer'
 
-    within '.answers' do
-      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+      within '.answers' do
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end
+
+    scenario 'delete file from answer', js: true do
+      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      click_on 'Add answer'
+
+      click_on 'Delete file'
+
+      within '.answers' do
+        expect(page).to_not have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end
+
+    scenario 'add several files when make asnwer', js: true do
+      click_on 'add file'
+
+      within(all(:css, '.nested-fields').first) do
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      end
+      within(all(:css, '.nested-fields').last) do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+      click_on 'Add answer'
+
+      within '.answers' do
+        expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+        expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
+      end
+    end
+  end
+
+  describe 'Non owner' do
+    given(:another_user) { create :user }
+    given(:answer) { create(:answer, question: question, user: another_user) }
+    given!(:answer_file) { create(:attachment, attachable: answer) }
+
+    scenario 'not to see delete file link' do
+      sign_in(user)
+      visit question_path(question)
+
+      within '.answers' do
+        expect(page).to_not have_link 'Delete file'
+      end
     end
   end
 end
--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -8,34 +8,44 @@ feature 'Add files to question', '
 
   given(:user) { create :user }
 
-  background do
-    sign_in(user)
-    visit new_question_path
-  end
+  describe 'User try' do
+    before do
+      sign_in(user)
+      visit new_question_path
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Content', with: 'text text text'
+    end
 
-  scenario 'User adds file when asks question' do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Content', with: 'text text text'
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    click_on 'Create'
-
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
-  end
-
-  scenario 'User add several files when asks question', js: true do
-    fill_in 'Title', with: 'Test question'
-    fill_in 'Content', with: 'text text text'
-    click_on 'add file'
-
-    within(all(:css, '.nested-fields').first) do
+    scenario 'add file when asks question' do
       attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
-    end
-    within(all(:css, '.nested-fields').last) do
-      attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
-    end
-    click_on 'Create'
+      click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
-    expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    end
+
+    scenario 'add several files when asks question', js: true do
+      click_on 'add file'
+
+      within(all(:css, '.nested-fields').first) do
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      end
+      within(all(:css, '.nested-fields').last) do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+      click_on 'Create'
+
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/3/rails_helper.rb'
+    end
+  end
+
+  describe 'Non owner' do
+    given(:another_user) { create :user }
+    given(:question) { create(:question, user: another_user) }
+    given!(:question_file) { create(:attachment, attachable: question) }
+
+    scenario 'not to see delete file link' do
+      expect(page).to_not have_link 'Delete file'
+    end
   end
 end
