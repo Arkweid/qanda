c719c953c3959ebf12f8dfcb49957b3ed55d196e
Arkweid <a.a.abroskin@yandex.ru>
2016-09-16 17:20:31 +0300

    fix edit answe nested form

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -10,4 +10,11 @@ class Answer < ActiveRecord::Base
   validates :content, length: { in: 10..1000 }
 
   accepts_nested_attributes_for :attachments, reject_if: :blank_file, allow_destroy: true
+
+  def switch_best
+    Answer.transaction do
+      Answer.where(question_id: question.id).update_all(best: false) unless best
+      toggle!(:best)
+    end
+  end  
 end
--- app/models/concerns/additional_methods.rb
+++ app/models/concerns/additional_methods.rb
@@ -4,11 +4,4 @@ module AdditionalMethods
   def blank_file(attributes)
     attributes['file'].blank?
   end
-
-  def switch_best
-    Answer.transaction do
-      Answer.where(question_id: question.id).update_all(best: false) unless best
-      toggle!(:best)
-    end
-  end
 end
