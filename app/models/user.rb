c87ff57beb28e468afe45371e157f18d5b15dcd1
Arkweid <a.a.abroskin@yandex.ru>
2016-09-20 16:19:06 +0300

    add methods for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   def author_of?(object)
     object.user_id == id
   end
+
+  #true if user voted
+  def voted?(object)
+    object.votes.where(user: self).exists?
+  end
+
+  #true if user can vote
+  def can_vote?(object)
+    !author_of?(object) && !voted?(object)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,41 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+
+  let(:user) { create :user }
+  let(:another_user) { create :user }
+  let(:question) { create :question, user: user }
+
+  describe '#author_of?' do
+    it 'user is author' do
+      expect(user).to be_author_of(question)
+    end
+
+    it 'user non author' do
+      expect(another_user).to_not be_author_of(question)
+    end
+  end
+
+  describe '#voted?' do
+    it 'user already voted' do
+      question.set_evaluate(user, 1)
+
+      expect(user).to be_voted(question)
+    end
+
+    it 'user not vote yet' do
+      expect(user).to_not be_voted(question)
+    end
+  end
+
+  describe '#can_vote?' do
+    it 'author can`t vote' do
+      expect(user).to_not be_can_vote(question)
+    end
+
+    it 'non author can vote' do
+      expect(another_user).to be_can_vote(question)
+    end
+  end
 end
