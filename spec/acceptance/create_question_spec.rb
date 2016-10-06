c9d8ed1f23af4aef1d0278670fa70b7bc928fddb
Arkweid <a.a.abroskin@yandex.ru>
2016-10-06 14:56:57 +0300

    add cancan functionality in views

--- spec/acceptance/create_question_spec.rb
+++ spec/acceptance/create_question_spec.rb
@@ -47,9 +47,8 @@ feature 'Create question', '
   context 'User non-registred' do
     scenario 'Non-authenticated user create question' do
       visit questions_path
-      click_on 'Ask question'
 
-      expect(page).to have_content 'You need to sign in or sign up before continuing.'
+      expect(page).to_not have_content 'Ask question'
     end
   end
 end
